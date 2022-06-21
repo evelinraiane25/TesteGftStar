2abea7c73d8708843f0700874f71003a03408397
Evelin Raiane <evelin_raiane@hotmail.com>
2022-06-21 17:57:52 -0300

    adicona projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteTecnicoGftStart
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
